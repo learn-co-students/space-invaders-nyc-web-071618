d767cbda354f27bf9893b1bdc8d72db9a5fdda81
trisjones <trisjones04@gmail.com>
2018-08-29 17:46:28 -0400

    Done.

--- src/spaceship.js
+++ src/spaceship.js
@@ -1,1 +1,22 @@
-class Spaceship {}
+
+class Spaceship {
+
+  constructor(name, crew, phasers, shields){
+    this.name = name
+    this.crew = crew
+    this.phasers = 5
+    this.shields = 4
+    this.cloaked = false
+    this.warpDrive = `disengaged`
+    this.phasersCharge = `uncharged`
+    this.assignCrew()
+    crew.length !== 0 ? this.docked = false : this.docked = true
+  }
+
+  assignCrew(){
+    this.crew.forEach((crew) => crew.currentShip = this)
+  }
+
+}
+
+stellarMobile = new Spaceship("Stellar Mobile")
