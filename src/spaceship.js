3ab71cf8a842b8e87c78d5f3bf13b267a8c07135
jin li <jinyuli91@gmail.com>
2018-08-29 21:24:30 -0400

    Done.

--- src/spaceship.js
+++ src/spaceship.js
@@ -1,1 +1,14 @@
-class Spaceship {}
+class Spaceship {
+  constructor(name, crew, phasers, shields,){
+    this.name = name;
+    this.crew = crew;
+    crew.forEach(mem => mem.currentShip = this)
+    this.phasers = phasers;
+    this.shields = shields;
+    this.cloaked = false;
+    this.warpDrive = "disengaged";
+    this.phasersCharge = 'uncharged';
+    crew.length > 0 ? this.docked = false : this.docked = true;
+
+  }
+}
